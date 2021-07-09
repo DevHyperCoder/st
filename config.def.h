a4489de30c4fe7f11d66a62d731ce64c75b4f8c9
DevHyperCoder <arduinoleo88@gmail.com>
2021-07-09 09:26:09 +0530

    update theme

--- config.def.h
+++ config.def.h
@@ -96,36 +96,56 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.95;
+float alpha = 0.90;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#282a36",
-	"#ff5555",
-	"#50fa7b",
-	"#f1fa8c",
-	"#bd93f9",
-	"#ff79c6",
-	"#8be9fd",
-	"#bbbbbb",
+	"#3b4252", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#88c0d0", /* cyan    */
+	"#e5e9f0", /* white   */
+    
+	//"#282a36",
+	//"#ff5555",
+	//"#50fa7b",
+	//"#f1fa8c",
+	//"#bd93f9",
+	//"#ff79c6",
+	//"#8be9fd",
+	//"#bbbbbb",
 
 	/* 8 bright colors */
-	"#282a36",
-	"#ff5555",
-	"#50fa7b",
-	"#f1fa8c",
-	"#bd93f9",
-	"#ff79c6",
-	"#8be9fd",
-	"#ffffff",
+	//"#282a36",
+	//"#ff5555",
+	//"#50fa7b",
+	//"#f1fa8c",
+	//"#bd93f9",
+	//"#ff79c6",
+	//"#8be9fd",
+	//"#ffffff",
+
+	"#4c566a", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#8fbcbb", /* cyan    */
+	"#eceff4", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+	//"#cccccc",
+	//"#555555",
+    "#2e3440", /* background */
+	"#d8dee9", /* foreground */
+    "black",
 };
 
 
@@ -133,10 +153,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
